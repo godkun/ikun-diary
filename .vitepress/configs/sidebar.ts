cd2fc31e91380d22ae593a916e4c101e10a2e30e
godkun <godkun666@126.com>
2024-05-02 21:33:40 +0800

    docs: update

--- .vitepress/configs/sidebar.ts
+++ .vitepress/configs/sidebar.ts
@@ -219,6 +219,10 @@ export const sidebar = {
       collapsed: false,
       items: [
         {
+          text: "大话代码架构",
+          link: "/blog/读书笔记/大话代码架构/index.md",
+        },
+        {
           text: "从零开始学架构",
           link: "/blog/读书笔记/从零开始学架构/index.md",
         },
